e43ede3d6c6d60b81727ed365ae71b128157f8ea
xianwew <xianwei@vt.edu>
2025-05-13 17:56:02 -0400

    data file, db and tests

--- index/index.go
+++ index/index.go
@@ -6,10 +6,39 @@ import (
 	"github.com/google/btree"
 )
 
+// Indexer 抽象索引接口，后续如果想要接入其他的数据结构，则直接实现这个接口即可
 type Indexer interface {
+	// Put 向索引中存储 key 对应的数据位置信息
 	Put(key []byte, pos *data.LogRecordPos) bool
+
+	// Get 根据 key 取出对应的索引位置信息
 	Get(key []byte) *data.LogRecordPos
+
+	// Delete 根据 key 删除对应的索引位置信息
 	Delete(key []byte) bool
+}
+
+type IndexType = int8
+
+const (
+	// Btree 索引
+	Btree IndexType = iota + 1
+
+	// ART 自适应基数树索引
+	ART
+)
+
+// NewIndexer 根据类型初始化索引
+func NewIndexer(typ IndexType) Indexer {
+	switch typ {
+	case Btree:
+		return NewBTree()
+	case ART:
+		// todo
+		return nil
+	default:
+		panic("unsupported index type")
+	}
 }
 
 type Item struct {
---
+++ options.go
@@ -0,0 +1,34 @@
+package NosqlKVBitcask
+
+import "os"
+
+type Options struct {
+	// 数据库数据目录
+	DirPath string
+
+	// 数据文件的大小
+	DataFileSize int64
+
+	// 每次写数据是否持久化
+	SyncWrites bool
+
+	// 索引类型
+	IndexType IndexerType
+}
+
+type IndexerType = int8
+
+const (
+	// BTree 索引
+	BTree IndexerType = iota + 1
+
+	// ART Adpative Radix Tree 自适应基数树索引
+	ART
+)
+
+var DefaultOptions = Options{
+	DirPath:      os.TempDir(),
+	DataFileSize: 256 * 1024 * 1024, // 256MB
+	SyncWrites:   false,
+	IndexType:    BTree,
+}
